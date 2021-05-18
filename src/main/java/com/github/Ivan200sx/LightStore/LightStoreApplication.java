e169ab30b20c12886bca6bfe044c22fed8504ad7
Bykov <ivan.bykom@iae.lt>
2021-05-18 15:27:20 +0300

    second commit

--- src/main/java/com/github/Ivan200sx/LightStore/LightStoreApplication.java
+++ src/main/java/com/github/Ivan200sx/LightStore/LightStoreApplication.java
@@ -3,9 +3,11 @@ package com.github.Ivan200sx.LightStore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import com.github.Ivan200sx.LightStore.ui.MainView;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class LightStoreApplication {
+public class LightStoreApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LightStoreApplication.class, args);
