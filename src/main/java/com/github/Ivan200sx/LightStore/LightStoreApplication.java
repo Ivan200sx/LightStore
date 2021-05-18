80c4b7ef76c51dbd1c27b17bfdf02140624f709c
Bykov <ivan.bykom@iae.lt>
2021-05-18 11:30:56 +0300

    first commit

---
+++ src/main/java/com/github/Ivan200sx/LightStore/LightStoreApplication.java
@@ -0,0 +1,13 @@
+package com.github.Ivan200sx.LightStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/Ivan200sx/LightStore/LightStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Ivan200sx.LightStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
